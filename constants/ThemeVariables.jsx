e4781c27f158f797f86bf0a702cceeee85f2af99
Alberte Illum <143408303+illumss@users.noreply.github.com>
2024-07-02 11:05:38 +0200

    Bit more colors

--- constants/ThemeVariables.jsx
+++ constants/ThemeVariables.jsx
@@ -4,5 +4,16 @@ const button = "#F3A61E";
 const lowkey = "#6B6B6B";
 const positive = "#258C00";
 const negative = "#BE1D29";
+const white = "#fff";
+const textcolor = "#000";
 
-export { primary, secondary, button, lowkey, positive, negative };
+export {
+  primary,
+  secondary,
+  button,
+  lowkey,
+  positive,
+  negative,
+  white,
+  textcolor,
+};
