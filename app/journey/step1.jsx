9d2c677ba92591947b632621a5cbe9b01b5adcdf
Alberte Illum <143408303+illumss@users.noreply.github.com>
2024-07-02 10:56:15 +0200

    Implemented ColorTheme

--- app/journey/step1.jsx
+++ app/journey/step1.jsx
@@ -1,34 +1,40 @@
-import { StyleSheet, TouchableOpacity } from 'react-native';
-import { Text, View} from '@/components/Themed';
-import { TextInput} from 'react-native';
+import { StyleSheet, TouchableOpacity } from "react-native";
+import { Text, View } from "@/components/Themed";
+import { TextInput } from "react-native";
 import { router } from "expo-router";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 export default function Journey1Step() {
-  const [location, setLocation] = useState("")
+  const [location, setLocation] = useState("");
   let data = {
     location: "",
-  }
+  };
 
   function handleGoStep2() {
-    data.location = location
+    data.location = location;
     router.push({
       pathname: "/journey/step2",
-      params: { data: JSON.stringify(data) }
+      params: { data: JSON.stringify(data) },
     });
   }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Step 1 - Location</Text>
-      <TextInput 
-      onChangeText={setLocation} 
-      value={location}
-      placeholder="Type your location"/>
+      <Text style={styles.number}>1</Text>
+      <Text style={styles.title}>Location</Text>
+      <TextInput
+        onChangeText={setLocation}
+        value={location}
+        placeholder="Type your location"
+      />
       <TouchableOpacity onPress={handleGoStep2}>
         <Text>Go to step 2</Text>
       </TouchableOpacity>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
     </View>
   );
 }
@@ -36,16 +42,21 @@ export default function Journey1Step() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  number: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "blue",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
---
+++ constants/ThemeVariables.jsx
@@ -0,0 +1,8 @@
+const primary = "#1D80C3";
+const secondary = "#E6EFFC";
+const button = "#F3A61E";
+const lowkey = "#6B6B6B";
+const positive = "#258C00";
+const negative = "#BE1D29";
+
+export { primary, secondary, button, lowkey, positive, negative };
