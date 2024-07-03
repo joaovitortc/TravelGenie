35bb31f1945c119a5e2ce9afc1c1c407d25fa583
Alberte Illum <143408303+illumss@users.noreply.github.com>
2024-07-03 19:17:23 +0200

    Step six first version

--- app/journey/step6.jsx
+++ app/journey/step6.jsx
@@ -1,33 +1,138 @@
-import { StyleSheet, TouchableOpacity } from "react-native";
-import { Text, View } from "@/components/Themed";
-import { router, useLocalSearchParams } from "expo-router";
-import { TextInput } from "react-native";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, TouchableOpacity, Text, View } from "react-native";
+import { router } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import {
+  primary,
+  secondary,
+  white,
+  lowkey,
+  button,
+  black,
+} from "../../constants/ThemeVariables";
 
-export default function Journey4Step() {
-  const [party, setParty] = useState("");
-  let { data } = useLocalSearchParams();
-  data = data ? JSON.parse(data) : {};
+const ProgressBar = ({ currentStep, totalSteps }) => {
+  const progressPercentage = (currentStep / totalSteps) * 100;
 
-  function handleGoStep6() {
-    data.party = party;
+  return (
+    <View style={styles.progressContainer}>
+      <View style={[styles.progressBar, { width: `${progressPercentage}%` }]} />
+    </View>
+  );
+};
+
+export default function Journey1Step() {
+  const [location] = useState("");
+  const [selectedAmount, setSelectedAmount] = useState(null);
+  const [selectedAge, setSelectedAge] = useState(null);
+  const currentStep = 6;
+  const totalSteps = 6;
+  let data = {
+    location: "",
+    budget: "",
+  };
+
+  function handleGoNextStep() {
+    data.amount = selectedAmount;
+    data.age = selectedAge;
     router.push({
       pathname: "/journey/results",
       params: { data: JSON.stringify(data) },
     });
   }
 
+  function handleGobackAStep() {
+    router.back("/");
+  }
+
+  const amountOptions = ["1", "2", "3", "4", "5", "6", "7-10", "10+"];
+  const ageOptions = ["0-10", "11-15", "16-20", "21-30", "31-50", "51-80"];
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Step 6 - Party Size</Text>
-      <TextInput
-        onChangeText={setParty}
-        value={party}
-        placeholder="Preferences for the trip?"
-      />
-      <TouchableOpacity onPress={handleGoStep6}>
-        <Text>Go to step 6</Text>
+      <ProgressBar currentStep={currentStep} totalSteps={totalSteps} />
+      <View style={styles.titlecontainer}>
+        <View style={styles.circle}>
+          <Text style={styles.number}>6</Text>
+        </View>
+        <Text style={styles.title}>About your party</Text>
+      </View>
+      <Text style={styles.paragraph}>How many are you?</Text>
+
+      <View style={styles.moneyContainer}>
+        {amountOptions.map((option, index) => (
+          <TouchableOpacity
+            key={index}
+            style={[
+              styles.moneyButton,
+              selectedAmount === option && { backgroundColor: button },
+            ]}
+            onPress={() => setSelectedAmount(option)}>
+            <Text
+              style={[
+                styles.moneyText,
+                selectedAmount === option && { color: black },
+              ]}>
+              {option}
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+
+      <Text style={styles.paragraph}>
+        What is the age range? You can select multiple
+      </Text>
+
+      <View style={styles.moneyContainer}>
+        {ageOptions.map((option, index) => (
+          <TouchableOpacity
+            key={index}
+            style={[
+              styles.moneyButton,
+              selectedAge === option && { backgroundColor: button },
+            ]}
+            onPress={() => setSelectedAge(option)}>
+            <Text
+              style={[
+                styles.moneyText,
+                selectedAge === option && { color: black },
+              ]}>
+              {option}
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+
+      <View style={styles.navigationbuttons}>
+        <TouchableOpacity
+          onPress={handleGobackAStep}
+          style={styles.navigationButtonBack}>
+          <Ionicons
+            name="arrow-back-outline"
+            size={20}
+            color={lowkey}
+            paddingTop={5}
+          />
+          <Text style={styles.navigationButtonTextBack}>Back</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={handleGoNextStep}
+          style={styles.navigationButtonNext}>
+          <Text style={styles.navigationButtonTextNext}>Next</Text>
+          <Ionicons
+            name="arrow-forward-outline"
+            size={20}
+            color={black}
+            paddingTop={5}
+          />
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity
+        onPress={handleGoNextStep}
+        style={styles.navigationButtonSkip}>
+        <Text style={styles.navigationButtonTextSkip}>Skip this step</Text>
       </TouchableOpacity>
+
       <View
         style={styles.separator}
         lightColor="#eee"
@@ -42,14 +147,139 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: secondary,
+    padding: 20,
+  },
+  titlecontainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    alignSelf: "flex-start",
+    paddingLeft: 32,
+    marginBottom: 20,
+    backgroundColor: secondary,
+  },
+  circle: {
+    width: 45,
+    height: 45,
+    borderRadius: 25,
+    backgroundColor: primary,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  number: {
+    color: white,
+    fontSize: 25,
+    fontWeight: "bold",
   },
   title: {
-    fontSize: 20,
+    fontSize: 25,
     fontWeight: "bold",
+    marginLeft: 20,
+  },
+  paragraph: {
+    fontSize: 14,
+    color: black,
+    marginBottom: 10,
+    width: "80%",
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: "80%",
+  },
+  progressContainer: {
+    width: "80%",
+    height: 10,
+    backgroundColor: "#e0e0e0",
+    borderRadius: 5,
+    marginBottom: 70,
+    overflow: "hidden",
+  },
+  progressBar: {
+    height: "100%",
+    backgroundColor: primary,
+  },
+  input: {
+    height: 40,
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: white,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    width: "80%",
+    backgroundColor: white,
+    fontSize: 14,
+  },
+  moneyButton: {
+    backgroundColor: white,
+    padding: 10,
+    borderRadius: 18,
+    marginBottom: 20,
+    width: "30%",
+  },
+  moneyText: {
+    color: black,
+    fontSize: 11,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  moneyContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    flexWrap: "wrap",
+    width: "80%",
+  },
+  orStyle: {
+    color: black,
+    marginBottom: 20,
+    fontWeight: "bold",
+    fontSize: 14,
+  },
+  navigationButtonBack: {
+    borderColor: lowkey,
+    borderWidth: 2,
+    padding: 10,
+    borderRadius: 30,
+    width: "45%",
+    marginTop: 80,
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  navigationButtonTextBack: {
+    color: lowkey,
+    fontSize: 14,
+    fontWeight: "bold",
+    textAlign: "center",
+    padding: 5,
+  },
+  navigationButtonNext: {
+    backgroundColor: button,
+    padding: 10,
+    borderRadius: 30,
+    width: "45%",
+    marginTop: 80,
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  navigationButtonTextNext: {
+    color: black,
+    fontSize: 14,
+    fontWeight: "bold",
+    textAlign: "center",
+    padding: 5,
+  },
+  navigationbuttons: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "80%",
+  },
+  navigationButtonSkip: {
+    marginTop: 30,
+  },
+  navigationButtonTextSkip: {
+    color: lowkey,
+    fontSize: 14,
+    textDecorationLine: "underline",
   },
 });
