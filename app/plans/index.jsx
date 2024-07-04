f456c05ef8afebefa52a92f84d6ca052c17aa631
Alberte Illum <143408303+illumss@users.noreply.github.com>
2024-07-04 12:59:09 +0200

    Styling

--- app/plans/index.jsx
+++ app/plans/index.jsx
@@ -1,10 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
 import { db, auth } from "../firebase";
-import { collection, doc, getDoc, updateDoc } from "firebase/firestore";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 import PlanCard from "@/components/PlanCard";
 import { router } from "expo-router";
 import Loading from "@/components/Loading";
+import {
+  black,
+  button,
+  primary,
+  white,
+  secondary,
+  lowkey,
+} from "@/constants/ThemeVariables";
 
 export default function Plans() {
   const [plans, setPlans] = useState([]);
@@ -92,32 +107,103 @@ export default function Plans() {
   if (empty) {
     return (
       <View>
+        <Text style={styles.title}>My plans</Text>
         <Text>No plans saved yet</Text>
-        <TouchableOpacity onPress={() => router.back()}>
-          <Text>Go back to home page</Text>
-        </TouchableOpacity>
+        <View style={styles.footer}>
+          <View style={styles.footerButtons}>
+            <TouchableOpacity
+              style={styles.footerButton}
+              onPress={() => router.push("/")}>
+              <Text style={styles.footerButtonText}>Home</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.footerButton}
+              onPress={() => router.push("/profile/page")}>
+              <Text style={styles.footerButtonText}>Profile</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     );
   }
 
   return (
-    <View>
-      {plans ? (
-        <FlatList
-          data={plans}
-          keyExtractor={(item, index) => index.toString()}
-          renderItem={({ item, index }) => (
-            <PlanCard
-              title={item.title}
-              plan={item.plan}
-              index={index}
-              onDelete={() => handleDelete(index)}
-            />
-          )}
-        />
-      ) : (
-        <Text>No plans saved yet</Text>
-      )}
-    </View>
+    <ScrollView style={styles.container}>
+      <Text style={styles.title}>My plans</Text>
+      <View>
+        {plans ? (
+          <FlatList
+            data={plans}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item, index }) => (
+              <PlanCard
+                title={item.title}
+                plan={item.plan}
+                index={index}
+                onDelete={() => handleDelete(index)}
+              />
+            )}
+          />
+        ) : (
+          <Text>No plans saved yet</Text>
+        )}
+
+        <View style={styles.footer}>
+          <View style={styles.footerButtons}>
+            <TouchableOpacity
+              style={styles.footerButton}
+              onPress={() => router.back("/")}>
+              <Text style={styles.footerButtonText}>Home</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.footerButton}
+              onPress={() => router.push("/profile/page")}>
+              <Text style={styles.footerButtonText}>Profile</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingTop: 20,
+    backgroundColor: secondary,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+    color: black,
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+  footer: {
+    marginTop: 40,
+    alignItems: "center",
+  },
+  footerText: {
+    fontSize: 14,
+    marginBottom: 10,
+  },
+  footerButtons: {
+    flexDirection: "row",
+  },
+  footerButton: {
+    borderColor: button,
+    borderWidth: 1,
+    borderRadius: 30,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginHorizontal: 10,
+  },
+  footerButtonText: {
+    color: black,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
